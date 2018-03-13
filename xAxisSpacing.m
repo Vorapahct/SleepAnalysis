1947f497fb1b31713149d90bce3b6f4f81d77224
Vorapahct <1068874@students.wits.ac.za>
2018-03-13 21:13:33 +0200

    Fixed Axis Issue

--- xAxisSpacing.m
+++ xAxisSpacing.m
@@ -1,6 +1,6 @@
 function x = xAxisSpacing(Type, intervalsOfType)
 
-    if(strcmp(Type,'wake') == true)
+    if(strcmp(Type,'wake EEG') == true)
         xSpacing = 0.5;
         xStep = 0.5;
     else
